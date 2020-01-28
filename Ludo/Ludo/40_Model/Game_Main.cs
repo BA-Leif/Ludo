44dd10f2f00af4c3598f55d9a43fdb8e8218ec04
LErichsen <lerichsen@consist.de>
2020-01-28 10:54:28 +0100

    View angepasst

---
+++ Ludo/Ludo/40_Model/Game_Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ludo._40_Model
+{
+    class Game_Main
+    {
+
+        private int ChangePlayer(int player)
+        {
+            int newPlayer = -1;
+            if (player >= 3)
+            {
+                newPlayer = 0;
+            }
+            else
+            {
+                newPlayer = player++;
+            }
+            return newPlayer;
+        }
+    }
+}
