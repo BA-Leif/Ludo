1c04cbc5ce2632a36f477a4216cc74aea952164e
LErichsen <lerichsen@consist.de>
2020-01-28 12:16:18 +0100

    Test eingebunden

--- Ludo/Ludo/40_Model/Game_Main.cs
+++ Ludo/Ludo/40_Model/Game_Main.cs
@@ -3,24 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ludo._20_Data;
 
 namespace Ludo._40_Model
 {
-    class Game_Main
+    public class Game_Main
     {
+        #region Eigenschaften und Konstruktor
+        public GameState GameState { get; set; }
 
-        private int ChangePlayer(int player)
+
+        public Game_Main(int player)
+        {
+            this.GameState = new GameState(player);
+        }
+        #endregion
+
+        public void ChangePlayer()
         {
             int newPlayer = -1;
-            if (player >= 3)
+            if (GameState.ActivePlayer >= 3)
             {
                 newPlayer = 0;
             }
             else
             {
-                newPlayer = player++;
+                newPlayer = GameState.ActivePlayer + 1;
             }
-            return newPlayer;
+            GameState.ActivePlayer = newPlayer;
         }
     }
 }
